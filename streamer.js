319ce5445fc0adaf5e67211d3ba12c551e66a0c3
rjp <zimpenfish@gmail.com>
2011-05-25 14:21:20 +0100

    - instead of fetching the JPEG in a client request, fetch it once and broadcast - every 10 seconds, fetch the webcam image and emit an event - HTTP client responses listen for the event and send that image

--- streamer.js
+++ streamer.js
@@ -12,6 +12,15 @@ var url = 'http://10.0.0.254/Jpeg/CamImg.jpg';
 
 var client = new hc.httpclient();
 
+setInterval(function(){
+    client.perform(url, "GET", function(result) {
+        var body = result.response.body;
+        var jl = body.length;
+        sys.puts("fetched JPEG of "+jl);
+        emitter.emit("webcam-image", jl, body);
+    }, null, {"Connection":"close", "x-binary": true}, null);
+}, 10000);
+
 http.createServer(function (req, res) {
     res.writeHead(200, {
         'Expires': "Thu, 1 Jan 1998 00:00:00 GMT",
@@ -19,18 +28,14 @@ http.createServer(function (req, res) {
     });
     sys.puts('Starting sending time');
     res.write("--mp-boundary\r\n");
-    setInterval(function(){
-        client.perform(url, "GET", function(result) { 
-            var body = result.response.body;
-            var jl = body.length;
-	        // sys.puts("sending "+jl+" bytes...");
-	        res.write(
-	            "Content-Type: image/jpeg\r\n"+
-	            "Content-Length: "+jl+"\r\n"+
-	            "\r\n"+ body +
-	            "\r\n--mp-boundary\r\n",
-	            "binary"
-	        );
-        }, null, {"Connection":"close", "x-binary": true}, null);
-    },10000);
+    emitter.addListener("webcam-image", function(jl, body) {
+        sys.puts("got a JPEG of "+jl);
+        res.write(
+            "Content-Type: image/jpeg\r\n"+
+            "Content-Length: "+jl+"\r\n"+
+            "\r\n"+ body +
+            "\r\n--mp-boundary\r\n",
+            "binary"
+        );
+    });
 }).listen(8090);
