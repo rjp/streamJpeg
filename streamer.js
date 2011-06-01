69ad818438e3a6c35e835e44ab7b9d876c7ff841
rjp <rjpartington@googlemail.com>
2011-06-01 11:55:12 +0100

    - only fetch the webcam image when we have active listeners - increment/decrement a counter when we start/stop a http request - FIXME this is still semi-broken in that it has too many event listeners

--- streamer.js
+++ streamer.js
@@ -11,17 +11,40 @@ var hc = require('./node-httpclient/lib/httpclient');
 var url = 'http://10.0.0.254/Jpeg/CamImg.jpg';
 
 var client = new hc.httpclient();
+var g_fetcher = undefined;
+var listeners = 0;
 
-setInterval(function(){
-    client.perform(url, "GET", function(result) {
-        var body = result.response.body;
-        var jl = body.length;
-        sys.puts("fetched JPEG of "+jl);
-        emitter.emit("webcam-image", jl, body);
-    }, null, {"Connection":"close", "x-binary": true}, null);
-}, 10000);
+emitter.addListener("start-streaming", function(){
+    listeners++;
+    sys.puts("we have "+listeners+" listeners, fetching");
+	g_fetcher = setInterval(function(){
+	    client.perform(url, "GET", function(result) {
+	        var body = result.response.body;
+	        var jl = body.length;
+	        sys.puts("fetched JPEG of "+jl);
+	        emitter.emit("webcam-image", jl, body);
+	    }, null, {"Connection":"close", "x-binary": true}, null);
+	}, 10000);
+    sys.puts("fetcher = "+sys.inspect(g_fetcher));
+});
+
+emitter.addListener("stop-streaming", function(){
+    if (listeners > 0) { 
+        listeners--;
+        sys.puts("stop-streaming: listeners == "+listeners);
+        if (listeners == 0 && g_fetcher !== undefined) {
+            sys.puts("clearing interval "+sys.inspect(g_fetcher));
+            clearInterval(g_fetcher);
+        }
+    }
+});    
 
 http.createServer(function (req, res) {
+    emitter.emit("start-streaming");
+    req.connection.addListener("end", function(){
+        sys.puts("http stream ended");
+        emitter.emit("stop-streaming");
+    });
     res.writeHead(200, {
         'Expires': "Thu, 1 Jan 1998 00:00:00 GMT",
         'Content-Type': 'multipart/x-mixed-replace; boundary=mp-boundary'
